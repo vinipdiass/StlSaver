7c3a443a74c57b5d325a0282e1767bb95cfcb0ec
Sonic-Cloud <Sonic_Cloud@hotmail.com>
2020-12-11 13:57:46 +0000

    Update herosaver_test.js

--- herosaver_test.js
+++ herosaver_test.js
@@ -292,7 +292,7 @@ function inject_script(url, callback) {
 console.log("HeroSaver - Injecting Libraries");
 
 inject_script("//code.jquery.com/jquery-3.3.1.min.js", function () {
-    inject_script("//cdn.jsdelivr.net/gh/carlalldis/StlSaver@master/three.js", function () { init() })
+    inject_script("//cdn.jsdelivr.net/gh/burndaflame/three.js@dev/build/three.module.js", function () { init() })
 });
 
 function get_name() {
