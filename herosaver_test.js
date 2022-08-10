6ee2b3c2946bfbc1aad292fe57a49ba0ddbca46e
turbolino <ldaleo10@gmail.com>
2022-08-10 14:46:16 +0200

    Update herosaver_test.js

--- herosaver_test.js
+++ herosaver_test.js
@@ -294,7 +294,7 @@ function inject_script(url, callback) {
 console.log("HeroSaver - Injecting Libraries");
 
 inject_script("//code.jquery.com/jquery-3.3.1.min.js", function () {
- inject_script("//cdn.jsdelivr.net/npm/three@0.139.2/build/three.min.js", function () { init() })
+ inject_script("//cdn.jsdelivr.net/npm/three@0.106.2/build/three.min.js", function () { init() })
 });
 //inject_script("//code.jquery.com/jquery-3.3.1.min.js", function () {
 //    inject_script("//cdn.jsdelivr.net/gh/burndaflame/three.js@dev/build/three.module.js", function () { init() })
